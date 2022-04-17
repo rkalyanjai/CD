ffdfc9fdb6965aed9ee875c3356b640c50987781
KalluSaviour <103513220+KalluSaviour@users.noreply.github.com>
2022-04-17 21:03:23 +0530

    Update Intermediate_Code_Generation.cpp

--- Lab5/Intermediate_Code_Generation.cpp
+++ Lab5/Intermediate_Code_Generation.cpp
@@ -93,3 +93,33 @@ T = 6
 A = c-T
 B = b+A
 a = B
+// Alg
+- Start the process.
+- Input an expression EXP from user.
+- Process the expression from right hand side to left hand side.
+- FLAG:=0; TOP = -1;
+- IF EXP = ‘=’ then
+     i. IF EXP(index – 1) = 0 then
+        1. PRINT EXP element from index to (index – 1) and POP STACK[TOP].
+        Terminate
+  Else
+     i. PRINT Wrong Expression
+  [EndIF]
+  IF an operator is found and FLAG = 0 then
+     i. TOP:= TOP + 1
+     ii. add to STACK[TOP].
+     iii. FLAG:=1
+  Else
+    i. pop twice the STACK and result add to the newID(identifier) and PRINT.
+    ii. TOP:=TOP-2. Save newID to STACK[TOP]
+    iii. FLAG:=0
+  [EndIF]
+- IF an operand is found then
+    i. TOP:=TOP+1
+    ii. move to STACK [TOP]
+    iii. IF TOP &gt; 1 then
+       1. pop twice the STACK and result add to the newID(identifier) and PRINT.
+       2. TOP:=TOP-2. Save newID to STACK[TOP]
+       3. FLAG:=0
+[End]
+- End the process
